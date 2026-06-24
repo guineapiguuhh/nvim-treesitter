72ae82fe61348372d025dc74c5191443fe9af269
GuineaPigUuhh <pedromaxaretz@gmail.com>
2026-06-24 19:51:47 -0300

    Update D Query

---
+++ _d.d
@@ -0,0 +1,25 @@
+module a.b.c.d;
+
+template to(int a)
+{
+    
+}
+
+enum neymar = 5;
+
+enum ZZZ
+{
+    a,
+    b,
+    c
+}
+
+void main(string[] args) {
+  auto _ = args;
+  auto x = q{};
+  auto y = q{
+    void main() {
+
+    }
+  };
+}
